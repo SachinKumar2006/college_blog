bc44463403eacb4f1aef0a48dd09e7f78d6f21a0
Sachin Kumar <sk9013797@gmail.com>
2026-03-08 16:13:04 +0530

    set debug false

--- ASSIGNMENT_1_DJANGO/college_blog/college_project/college_project/settings.py
+++ ASSIGNMENT_1_DJANGO/college_blog/college_project/college_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hyiif8eutpnm22d71-4z@6xpvs(qev#w)9kttepo_95siuyyl2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['college-blog-1.onrender.com']
 
