e659bc15112dcb76d9e3bc6ccdae2b26f0decba8
Sachin Kumar <sk9013797@gmail.com>
2026-03-08 18:02:40 +0530

    enable global templates folder

--- ASSIGNMENT_1_DJANGO/college_blog/college_project/college_project/settings.py
+++ ASSIGNMENT_1_DJANGO/college_blog/college_project/college_project/settings.py
@@ -56,18 +56,19 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'college_project.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+{
+'BACKEND': 'django.template.backends.django.DjangoTemplates',
+'DIRS': [BASE_DIR / "templates"],
+'APP_DIRS': True,
+'OPTIONS': {
+'context_processors': [
+'django.template.context_processors.debug',
+'django.template.context_processors.request',
+'django.contrib.auth.context_processors.auth',
+'django.contrib.messages.context_processors.messages',
+],
+},
+},
 ]
 
 WSGI_APPLICATION = 'college_project.wsgi.application'
